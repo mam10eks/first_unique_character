6827b8aa115fe25dc0350f0bb6283cd11082b2f0
Maik Fröbe <maik44froebe@web.de>
2018-09-21 17:18:03 +0200

    TDD-Cycle-Green
    Implement initial exception-handling for the time-saving-solution

---
+++ src/main/java/com/github/mam10eks/unique_char_test/TimeSavingSolution.java
@@ -0,0 +1,17 @@
+package com.github.mam10eks.unique_char_test;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * 
+ * @author Maik Fröbe
+ *
+ */
+@UtilityClass
+public class TimeSavingSolution
+{
+	public static char determineFirstUniqueCharacter(String input)
+	{	
+		throw new IllegalArgumentException("Illegal input '"+ input +"'!");
+	}
+}
