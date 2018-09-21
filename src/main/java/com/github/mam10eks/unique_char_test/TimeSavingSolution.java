8d05f591ba5a090b4f39063152e540dfc613642c
Maik Fröbe <maik44froebe@web.de>
2018-09-21 17:27:29 +0200

    TDD-Cycle-Green
    Implement the implementation of the time-saving-solution

--- src/main/java/com/github/mam10eks/unique_char_test/TimeSavingSolution.java
+++ src/main/java/com/github/mam10eks/unique_char_test/TimeSavingSolution.java
@@ -1,5 +1,8 @@
 package com.github.mam10eks.unique_char_test;
 
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
+
 import lombok.experimental.UtilityClass;
 
 /**
@@ -11,7 +14,39 @@ import lombok.experimental.UtilityClass;
 public class TimeSavingSolution
 {
 	public static char determineFirstUniqueCharacter(String input)
-	{	
-		throw new IllegalArgumentException("Illegal input '"+ input +"'!");
+	{
+		var charactersToCount = mapCharactersAfterTheirFirstOccurenceToCount(input);
+		
+		return determineFirstCharacterWithCountOneOrFail(charactersToCount);
+	}
+	
+	private static LinkedHashMap<Character, Integer> mapCharactersAfterTheirFirstOccurenceToCount(String input)
+	{
+		var ret = new LinkedHashMap<Character, Integer>();
+		
+		for(char c : input.toCharArray())
+		{
+			if(!ret.containsKey(c))
+			{
+				ret.put(c, 0);
+			}
+			
+			ret.put(c, ret.get(c) +1);
+		}
+		
+		return ret;
+	}
+	
+	private static char determineFirstCharacterWithCountOneOrFail(LinkedHashMap<Character, Integer> charactersToCount)
+	{
+		for(Entry<Character, Integer> characterToCount : charactersToCount.entrySet())
+		{
+			if(characterToCount.getValue() == 1)
+			{
+				return characterToCount.getKey();
+			}
+		}
+		
+		throw new IllegalArgumentException("Illegal input!");
 	}
 }
