d4d9a7b905fafb154d1cac782f3d8ac1d7fb8f42
Maik Fröbe <maik44froebe@web.de>
2018-09-21 16:56:20 +0200

    TDD-Cycle-Red
    Add first failing test to check that an exception is thrown for illegal inputs

---
+++ src/test/java/com/github/mam10eks/unique_char_test/IllegalInputTest.java
@@ -0,0 +1,41 @@
+package com.github.mam10eks.unique_char_test;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author Maik Fröbe
+ *
+ */
+@Data
+@RunWith(Parameterized.class)
+public class IllegalInputTest
+{
+	private final String illegalInput;
+	
+	@Parameters
+	public static Collection<String[]> parameters()
+	{
+		return Arrays.asList(
+				new String[] {null},
+				new String[] {""},
+				new String[] {"aa"},
+				new String[] {"aabb"},
+				new String[] {"abab"},
+				new String[] {"abba"});
+	}
+	
+	@Test(expected=Exception.class)
+	public void checkThatIllegalInputCausesExceptionForMemorySavingSolution()
+	{
+		MemorySavingSolution.determineFirstUniqueCharacter(illegalInput);
+	}
+}
